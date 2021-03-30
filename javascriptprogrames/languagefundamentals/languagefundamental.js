cea9eded0d94c747ad92b9af597afb7a99f77d7c
derin <derinkochappu1996@gmail.com>
2021-03-30 19:04:57 +0530

    adding files

---
+++ javascriptprogrames/languagefundamentals/languagefundamental.js
@@ -0,0 +1,8 @@
+var company="luminar technololab";
+console.log("our company is "+company)
+var myname="derin kochappu";
+vae age="25";
+console.log("hai lam"+ myname)
+var company="luminar";
+var location="kakkanad";
+console.log(`our company ${company} located in ${location}`);
